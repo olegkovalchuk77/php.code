bbd23cad197fb26986a149541e759543bd3684ab
root <okovalchuk@gmail.com>
2017-04-01 16:49:12 +0300

    add html code

---
+++ index.php
@@ -0,0 +1,34 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<table border="1">
+    <tr>
+        <td>phpschol - lesson 1</td>
+    </tr>
+</table>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: okovalchuk
+ * Date: 01.04.17
+ * Time: 15:57
+ */
+
+phpinfo();
+
+?>
+
+
+
+</body>
+</html>
+
+
+
+
